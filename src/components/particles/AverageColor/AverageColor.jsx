4492f572bec083962d26748480134e3a0ab7f163
Valentin <valaimg2218@gmail.com>
2022-12-24 23:01:43 +0200

    builded app

--- src/components/particles/AverageColor/AverageColor.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AverageColor = ({color}) => {
-    const {red, green, blue} = color;
-
-    return <h3>The average color is: rgb({red}, {green}, {blue})</h3>
-};
-
-export default AverageColor;
--- src/components/particles/DominantColor/DominantColor.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const DominantColor = ({color}) => <h3>Dominant color is: {color}</h3>
-
-export default DominantColor;
